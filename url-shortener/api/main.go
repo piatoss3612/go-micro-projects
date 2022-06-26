9a6780cfbe6d30b424d4531ad44ad66bf3e562b3
Hyohwak Lee <crewe1746@naver.com>
2022-06-26 13:42:01 +0900

    URL Shortener PART-1

---
+++ url-shortener/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
+
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	setupRoutes(app)
+
+	if err = app.Listen(os.Getenv("APP_PORT")); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
