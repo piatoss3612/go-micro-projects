f2c4ce68f0939c84fc50e40af0b7b3afbcfae673
Hyohwak Lee <crewe1746@naver.com>
2022-06-26 14:43:59 +0900

    URL Shortener PART-3

--- url-shortener/api/routes/resolove.go
+++ url-shortener/api/routes/resolove.go
@@ -1,7 +1,28 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"url-shortener/database"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
 
 func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(
+			fiber.Map{"error": "short not found in the database"},
+		)
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			fiber.Map{"error": "cannot connect to DB"},
+		)
+	}
+
 	return nil
 }
